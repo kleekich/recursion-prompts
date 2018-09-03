f96a662d750d439b866b328a7edf564db7cdde25
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-03 15:09:14 -0700

    Implements Integer Range

--- src/recursion.js
+++ src/recursion.js
@@ -57,6 +57,10 @@ var sumBelow = function(n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
+	var arr = [];
+	if(x===y|| x+1 === y || x-1 === y) return arr;
+	if(x>y) return arr.concat(x-1, range(x-1, y));
+	else return arr.concat(x+1, range(x+1,y));
 };
 
 // 7. Compute the exponent of a number.
