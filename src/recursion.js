deb88a5be7e4250f347ed1fb1524c2ba0615a10d
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-03 15:35:01 -0700

    Implements Palindrom

--- src/recursion.js
+++ src/recursion.js
@@ -94,6 +94,11 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+	string = string.trim().toLowerCase();
+	if(string.length === 0 || string.length === 1) return true;
+	if(string.charAt(0) !== string.charAt(string.length-1)) return false;
+	return palindrome(string.substring(1, string.length-1));
+
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
