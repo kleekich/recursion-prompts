c79c7eb85edcf76d41a2ff033cddf592f1d924db
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-03 19:36:30 -0700

    Implements minize zeroes

--- src/recursion.js
+++ src/recursion.js
@@ -353,6 +353,17 @@ var augmentElements = function(array, aug) {
 // minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
 // minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
 var minimizeZeroes = function(array) {
+	if(array.length === 0) return [];
+
+	if(array[0] === 0){
+		var i = 0;
+		while(array[i] === 0){
+			i++;
+		}
+		return [0].concat(minimizeZeroes(array.slice(i)));
+	}else{
+		return [array[0]].concat(minimizeZeroes(array.slice(1)));
+	}
 };
 
 // 35. Alternate the numbers in an array between positive and negative regardless of
