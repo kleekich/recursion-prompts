5dc11ea10e4d4e18e1a7a0b865994849ee2dd419
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-03 18:55:15 -0700

    Implements nth Fibonacci

--- src/recursion.js
+++ src/recursion.js
@@ -231,6 +231,16 @@ var countValuesInObj = function(obj, value) {
 // 24. Find all keys in an object (and nested objects) by a provided name and rename
 // them to a provided new name while preserving the value stored at that key.
 var replaceKeysInObj = function(obj, oldKey, newKey) {
+	if(!obj.hasOwnProperty(oldKey)) return obj;
+	else{
+		obj[newKey] = obj[oldKey];
+		delete obj[oldKey];
+	}
+	for(var key in obj){
+		if(typeof obj[key] === 'object') obj[key] = replaceKeysInObj(obj[key], oldKey, newKey);
+	}
+	
+	return obj;
 };
 
 // 25. Get the first n Fibonacci numbers. In the Fibonacci sequence, each subsequent
@@ -239,6 +249,9 @@ var replaceKeysInObj = function(obj, oldKey, newKey) {
 // fibonacci(5); // [0,1,1,2,3,5]
 // Note: The 0 is not counted.
 var fibonacci = function(n) {
+	if(n===0) return [0];
+	else if(n<=1) return [1];
+	else return fibonacci(n-2).concat(fibonacci(n-1)).concat([n]);
 };
 
 // 26. Return the Fibonacci number located at index n of the Fibonacci sequence.
@@ -247,6 +260,10 @@ var fibonacci = function(n) {
 // nthFibo(7); // 13
 // nthFibo(3); // 2
 var nthFibo = function(n) {
+	if(n <0 ) return null;
+	if(n === 0) return 0;
+	else if(n === 1) return 1;
+	else return nthFibo(n-2)+nthFibo(n-1);
 };
 
 // 27. Given an array of words, return a new array containing each word capitalized.
