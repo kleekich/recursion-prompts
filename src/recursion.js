493927dfa385326a5dbd3df5a4476c5f99e42f02
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-03 19:33:21 -0700

    Implements augment each eleme t in nested arrays

--- src/recursion.js
+++ src/recursion.js
@@ -344,6 +344,9 @@ var compress = function(list) {
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
 var augmentElements = function(array, aug) {
+	if(array.length === 0) return [];
+	
+	return [array[0].concat([aug])].concat(augmentElements(array.slice(1),aug));
 };
 
 // 34. Reduce a series of zeroes to a single 0.
