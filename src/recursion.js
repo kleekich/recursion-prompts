58d04cc5f24d827be5995d6b1790bb0d079f0939
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-03 18:11:29 -0700

    Implements Reverse array

--- src/recursion.js
+++ src/recursion.js
@@ -162,6 +162,8 @@ var createArray = function(str) {
 
 // 17. Reverse the order of an array
 var reverseArr = function(array) {
+	if(array.length===0) return [];
+	return [array[array.length-1]].concat(reverseArr(array.slice(0, array.length-1)));
 };
 
 // 18. Create a new array with a given value and length.
