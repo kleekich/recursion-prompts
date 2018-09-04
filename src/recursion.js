2cfe37e6f6ad2a371e4c6c872034cf06ab87e0ca
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-03 20:02:17 -0700

    Implements gcd

--- src/recursion.js
+++ src/recursion.js
@@ -139,7 +139,9 @@ var divide = function(x, y) {
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 var gcd = function(x, y) {
-
+	if (x < 0 || y < 0) return null; 
+  	if (y % x === 0) return x;
+  	return x > y ? gcd(y, x) : gcd(x, y % x);
 };
 
 // 15. Write a function that compares each character of two strings and returns true if
