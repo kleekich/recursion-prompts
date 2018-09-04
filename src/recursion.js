3cba55b49a052a90734386f4a759bc2b38bc1a9e
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-03 18:28:33 -0700

    Implement Count value in object

--- src/recursion.js
+++ src/recursion.js
@@ -207,6 +207,12 @@ var rMap = function(array, callback) {
 // countKeysInObj(obj, 'r') // 1
 // countKeysInObj(obj, 'e') // 2
 var countKeysInObj = function(obj, key) {
+	var sum = 0;
+	for(var k in obj){
+		if(k ===key) sum++;
+		if(typeof obj[k] === 'object') sum+= countKeysInObj(obj[k],key);
+	}
+	return sum;
 };
 
 // 23. Write a function that counts the number of times a value occurs in an object.
